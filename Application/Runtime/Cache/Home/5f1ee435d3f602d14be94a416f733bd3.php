7904bc9ca0658d5610039cbddec7666980fca9d6
hefju <hefju@qq.com>
2015-09-05 17:32:46 +0800

    郁闷, netbeans的提交会根据用户选择的文件夹来提交的. 不小心选择了子文件夹.这个跟idea不同啊.

---
+++ Application/Runtime/Cache/Home/5f1ee435d3f602d14be94a416f733bd3.php
@@ -0,0 +1,12 @@
+<?php if (!defined('THINK_PATH')) exit();?>
+   
+   <html>
+<head>
+<title>hello <?php echo ($name); ?></title>
+</head>
+<body>
+    hello, <?php echo ($name); ?>!
+    <br/>
+    应该访问视图才正确
+</body>
+</html>
